aaf4f6b9b7f923ed5b94056d1d9bef21d18717a6
A5245 <onDodA@outlook.com>
2025-09-23 22:10:27 +0800

    升级minSdk到26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "test.hook.debug"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
