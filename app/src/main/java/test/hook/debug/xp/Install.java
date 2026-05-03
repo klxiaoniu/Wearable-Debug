7ed1fc47238bc6f7ded73bc25e4014abb776bc54
klxiaoniu <klxiaoniu@proton.me>
2026-05-03 22:32:59 +0800

    修复新版本固件更新以及表盘安装，尝试兼容旧版本
    https://github.com/A5245/Wearable-Debug/commit/7f6656dd40c474569a07b0d9e500bd25795f7c8e

--- app/src/main/java/test/hook/debug/xp/Install.java
+++ app/src/main/java/test/hook/debug/xp/Install.java
@@ -48,6 +48,32 @@ public class Install {
     }
 
     /**
+     * 启动升级页面
+     *
+     * @param loader  当前类加载器
+     * @param context 上下文用于Intent
+     * @param path    固件文件位置
+     */
+    public static void invokeUpdate(ClassLoader loader, Object context, String path) {
+        try {
+            Class<?> checkUpdateExtKt = XposedHelpers.findClass("com.mi.fitness.checkupdate.export.CheckUpdateExtKt", loader);
+            Object manager = XposedHelpers.callStaticMethod(checkUpdateExtKt, "getCheckUpdateManager");
+
+            XposedHelpers.callMethod(manager, "manualUpgrade", new Class[]{Context.class, String.class, boolean.class},
+                    context, path, false);
+        } catch (Throwable e) {
+            Log.e(e, "invokeUpdate new version not found");
+
+            // 旧版本
+            Class<?> checkUpdateManagerImpl = XposedHelpers.findClass("com.mi.fitness.checkupdate.util.CheckUpdateManagerImpl", loader);
+            Object manager = XposedHelpers.newInstance(checkUpdateManagerImpl);
+            // boolean参数为true时为静默安装
+            XposedHelpers.callMethod(manager, "manualUpgrade", new Class[]{Context.class, String.class, boolean.class},
+                    context, path, false);
+        }
+    }
+
+    /**
      * 安装表盘文件
      *
      * @param loader  当前类加载器
@@ -62,9 +88,15 @@ public class Install {
                 return;
             }
 
-            Class<?> model = XposedHelpers.findClass("com.xiaomi.fitness.watch.face.viewmodel.FaceDetailViewModel", loader);
-            Object instance = model.newInstance();
+            Class<?> model;
+            try {
+                // 新
+                model = XposedHelpers.findClass("com.xiaomi.fitness.watch.face.viewmodel.FaceBleInfoViewModel", loader);
+            } catch (XposedHelpers.ClassNotFoundError e) {
+                model = XposedHelpers.findClass("com.xiaomi.fitness.watch.face.viewmodel.FaceDetailViewModel", loader);
+            }
 
+            Object instance = model.newInstance();
             Object controller = XposedHelpers.getObjectField(instance, "faceInstallController");
 
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
@@ -93,8 +125,13 @@ public class Install {
                             dialog.dismiss();
                             break;
                         }
-                        default:
-                            throw new IllegalStateException("Unexpected value: " + method.getName());
+                        case "onStart": {
+                            Log.i("start install", null);
+                            break;
+                        }
+                        default: {
+                            Log.e("Unknown value: " + method.getName(), null);
+                        }
                     }
                 } catch (Throwable e) {
                     Log.e(e, method.toString());
@@ -105,27 +142,40 @@ public class Install {
                 return null;
             });
 
-            XposedHelpers.callMethod(controller, "doInstall", new Class<?>[]{
-                    String.class, String.class, Integer.class, callbackClass
-            }, file.getAbsolutePath(), watchFaceId, 0, callback);
+            try {
+                // 构造 Kotlin 的 Function3 代理，用于 preInstall 回调
+                Class<?> function3Class = XposedHelpers.findClass("kotlin.jvm.functions.Function3", loader);
+                Object dummyAction = Proxy.newProxyInstance(loader, new Class<?>[]{function3Class}, (proxy, method, args) -> {
+                    // 安全起见，返回 kotlin.Unit.INSTANCE 以绕过 Kotlin 的非空检查
+                    return XposedHelpers.getStaticObjectField(XposedHelpers.findClass("kotlin.Unit", loader), "INSTANCE");
+                });
+
+                // 先调用 preInstall 建立握手
+                // preInstall(String id, String path, long version, long size, boolean needCheckStorage, String license, String sign, Integer trialDuration, Function3 action)
+                XposedHelpers.callMethod(controller, "preInstall",
+                        new Class<?>[]{
+                                String.class, String.class, long.class, long.class, boolean.class,
+                                String.class, String.class, Integer.class, function3Class
+                        },
+                        watchFaceId, file.getAbsolutePath(), 0L, file.length(), true,
+                        null, null, 0, dummyAction
+                );
+            } catch (Throwable error) {
+                // 旧版没这步骤
+            }
+
+            // 调用 doInstall 正式传输文件
+            // doInstall(String path, String id, Integer segmentLength, FaceInstallPushCallback callback)
+            XposedHelpers.callMethod(controller, "doInstall",
+                    new Class<?>[]{
+                            String.class, String.class, Integer.class, callbackClass
+                    },
+                    file.getAbsolutePath(), watchFaceId, 0, callback
+            );
+
         } catch (Throwable e) {
             Log.e(e, "installWatchFace");
         }
-    }
-
-    /**
-     * 启动升级页面
-     *
-     * @param loader  当前类加载器
-     * @param context 上下文用于Intent
-     * @param path    固件文件位置
-     */
-    public static void invokeUpdate(ClassLoader loader, Object context, String path) {
-        Class<?> checkUpdateManagerImpl = XposedHelpers.findClass("com.mi.fitness.checkupdate.util.CheckUpdateManagerImpl", loader);
-        Object manager = XposedHelpers.newInstance(checkUpdateManagerImpl);
-        // boolean参数为true时为静默安装
-        XposedHelpers.callMethod(manager, "manualUpgrade", new Class[]{Context.class, String.class, boolean.class},
-                context, path, false);
     }
 
     /**
@@ -157,16 +207,12 @@ public class Install {
      * @param thisObj 当前方法对象
      */
     public static Object unInstall(ClassLoader loader, Object thisObj) throws InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException {
-        if (Save.sign == null) {
-            return true;
-        }
         Object deviceModel = getCurrentDevice(loader);
         if (deviceModel == null) {
             return true;
         }
 
         Object did = XposedHelpers.callMethod(deviceModel, "getDid");
-
         Object pkgName = XposedHelpers.getObjectField(thisObj, "pkgName");
         Class<?> deviceModelExtKt = XposedHelpers.findClass("com.xiaomi.xms.wearable.extensions.DeviceModelExtKt", loader);
         Class<?> callback = XposedHelpers.findClass("com.xiaomi.xms.wearable.ui.debug.ThirdAppDebugFragment$unInstallApp$1", loader);
@@ -174,7 +220,14 @@ public class Install {
 
         Method uninstallApp = MethodFinder.fromClass(deviceModelExtKt).filterByName("uninstallApp").first();
         uninstallApp.setAccessible(true);
-        uninstallApp.invoke(deviceModelExtKt, deviceModel, pkgName, Save.sign, callbackObj);
+
+        // 如果 Save.sign 为空（未安装），造一个空的 20 字节签名数组
+        byte[] signToUse = Save.sign;
+        if (signToUse == null) {
+            signToUse = new byte[20]; // SHA-1 的长度
+        }
+
+        uninstallApp.invoke(deviceModelExtKt, deviceModel, pkgName, signToUse, callbackObj);
         return false;
     }
 
